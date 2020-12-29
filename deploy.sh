d2248538cb2220d60b6c3f44f8d3f211f9f60c23
qinzhiwei1993 <384579070@qq.com>
2020-12-29 20:36:37 +0800

    build dist

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 获取当前分支命令
+
+Get_Branch_Command=`git branch | grep "*"`
+
+# 获取当前分支名称
+Current_Branch=${Get_Branch_Command:2} 
+
+if [ $Current_Branch=="main" ]
+then  npm run test
+      git add .
+      git commit -m 'build dist'
+      git push
+fi
+
+
+
